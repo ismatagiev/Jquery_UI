a8e31cf3a5f8e4e48d2bd5d5c162437dd5b926d2
Isma Tagiev <tagiev.ismail2017@yandex.ru>
2022-03-14 12:29:34 +0700

    Задание 1 - Jquery_UI

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
-
-   $("#tabs").tabs();
-
+$(document).ready(function() {
+   $("#el1").draggable();
+   $("#el1").resizable();
+   $("#el2").draggable({stack:"#drop1"});
+   $("#el3").draggable({stack:"#drop1"});
+   $("#sort1").sortable();
+   $("#drop1").droppable({
+      drop:function(){  
+      $("#drop1").css("background-color", "#22b348");
+      $("#el2").css("display", "none")
+      }
+   });
+   $("#drop2").droppable({
+      accept: "#el3",
+      drop: function(){
+      $("#drop2").css("background-color", "#22b348");
+      $("#el3").css("display", "none")
+      }
+   })
 });
