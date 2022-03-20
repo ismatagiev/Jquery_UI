6d87556975edfc9ebe84563767c5853b2fdbb3a4
Isma Tagiev <tagiev.ismail2017@yandex.ru>
2022-03-20 14:13:23 +0700

    Сайт для к.н

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-$(document).ready(function() {
-   $("#el1").draggable();
-   $("#el1").resizable();
-   $("#el2").draggable({stack:"#drop1"});
-   $("#el3").draggable({stack:"#drop1"});
-   $("#sort1").sortable();
-   $("#drop1").droppable({
-      drop:function(){  
-      $("#drop1").css("background-color", "#22b348");
-      $("#el2").css("display", "none")
-      }
+function load_from_site() {
+   $get('http://217.71.129.139:4003/students.php', function(data) {
+      students = JSON.parse(data)['response']
    });
-   $("#drop2").droppable({
-      accept: "#el3",
-      drop: function(){
-      $("#drop2").css("background-color", "#22b348");
-      $("#el3").css("display", "none")
-      }
-   })
-});
+}
+
+$(document).ready(function(){
+
+   $("#tabs").tabs();
+
+}); 
